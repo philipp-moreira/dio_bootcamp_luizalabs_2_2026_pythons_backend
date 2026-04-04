38c768d08e5e6aa3a8ee076d6f8dd1c36b4754ac
moreira, philipp <psmdeveloper@gmail.com>
2026-04-04 14:12:45 -0300

    docs(operators): test behavior of association operator "in" with sequences (string and list)

---
+++ 01_onboarding_fundamentals_and_logic_programming/01_04_operator_types_with_python/01_04_06_association_operators/association_operators.py
@@ -0,0 +1,21 @@
+course_title = "Python's Course"
+curriculum_course = [
+    'operators',
+    'variables',
+    'conditional blocks',
+    'variablesnowhitespaces',
+]
+
+print('course_title = "Python\'s Course')
+print(
+    "curriculum_course = ['operators', 'variables', 'conditional\ blocks', 'variablesnowhitespaces'"
+)
+
+print(f'python in course_title ==> {"python" in course_title}')
+print(f'python in course_title ==> {"Python" in course_title}')
+print(f'p  in course_title ==> {"p" in course_title}')
+print(f'P  in course_title ==> {"P" in course_title}')
+print(f'o  in course_title ==> {"o" in course_title}')
+
+print(f'variable  in curriculum_course ==> {"variable" in curriculum_course}')
+print(f'variables  in curriculum_course ==> {"variables" in curriculum_course}')
